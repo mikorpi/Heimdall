3295632d05d17422652063c27d09064f75d448d2
mikorpi <73946058+mikorpi@users.noreply.github.com>
2020-12-15 19:45:18 +0200

    Update trainer.py

--- projekti/rpi/facerec/trainer.py
+++ projekti/rpi/facerec/trainer.py
@@ -2,28 +2,31 @@ import cv2
 import numpy as np
 from PIL import Image
 import os
-
+#polku datalle mitä tullaan hyödyntämään.
 path = 'dataset/path/to/pictures'
 
+#Local Binary Patterns Histograms. Haluaa datan grayscalekuvina.
 recognize = cv2.face.LBPHFaceRecognizer_create()
 detect = cv2.CascadeClassifier('dataset/haarcascade_frontalface_default.xml')
 
+#funktio joka toteuttaa opettamisen hyödyntäen numpy sekä PIL kirjastoa.
 def dostuff(path):
-    imagePaths = [os.path.join(path,i) for i in os.listdir(path)]
-    faceSamples=[]
+    imagepaths = [os.path.join(path,i) for i in os.listdir(path)]
+    facesamples=[]
     ids = []
 
-    for imagePath in imagePaths:
-        PIL_image = Image.open(imagePath).convert('L') #greyscale
-        image_numpy = np.array(PIL_image,'uint8')
-        id = int(os.path.split(imagePath)[-1].split(".")[1])
-        faces = detect.detectMultiScale(image_numpy)
+    for imagepath in imagepaths:
+        PIL_img = Image.open(imagepath).convert('L') #greyscale
+        img_numpy = np.array(PIL_img,'uint8')
+        id = int(os.path.split(imagepath)[-1].split(".")[1])
+        faces = detect.detectMultiScale(img_numpy)
         for (x,y,w,h) in faces:
-            faceSamples.append(image_numpy[y:y+h,x:x+w])
+            facesamples.append(img_numpy[y:y+h,x:x+w])
             ids.append(id)
-    return faceSamples,ids
-print("training in process")
+    return facesamples,ids
+#Prosessointi alkaa
+print("\n[i] training in process\n")
 faces,ids= dostuff(path)
 recognize.train(faces, np.array(ids))
 recognize.write('training/namethis.yml')
-print("treenit treenattu")
+print("\n[i] training done.\n")
